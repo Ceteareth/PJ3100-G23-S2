212a8b2ae2bdf62a0170f9acccdb34f7a2e7e5c8
Torkill Strømmen <strtor10@nith.no>
2012-11-29 14:38:05 +0100

    Updated scripts
    Updated door script to support other features, added flashlight script,
    added sound when turning on/off flashlight

--- CustomInteractionScript.cs
+++ CustomInteractionScript.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 using System.Collections;
 
-public class DoorOpenScript : MonoBehaviour {
+public class CustomInteractionScript : MonoBehaviour {
 	
 	public Object target;
-	public GameObject source;
-	public int triggerCount = 1;
-	public bool openDoor = false;
+	private bool openDoor = false;
 	public bool repeatTrigger = false;
 	public bool triggered = false;
+	public bool isDoor = true;
+	private int pickedPaintings = 0;
+	private bool displayMessage = false;
+	private bool itemPresent = true;
+	string message;
 	
 	void OpenDoor(){
-		triggerCount--;
 		
 		if (!openDoor || repeatTrigger){
 			Object currentTarget = target != null ? target : gameObject;
@@ -26,7 +28,6 @@ public class DoorOpenScript : MonoBehaviour {
 	}
 	
 	void CloseDoor(){
-		triggerCount++;
 		
 		if(openDoor || repeatTrigger){
 			Object currentTarget = target != null ? target : gameObject;
@@ -40,6 +41,29 @@ public class DoorOpenScript : MonoBehaviour {
 		}
 	}
 	
+	void PickUpPainting(){
+		Object currentTarget = target != null ? target : gameObject;
+		Behaviour targetBehaviour = currentTarget as Behaviour;
+		GameObject targetGameObject = currentTarget as GameObject;
+		if (targetBehaviour != null)
+			targetGameObject = targetBehaviour.gameObject;
+		
+		Destroy(targetGameObject);
+		pickedPaintings++;
+		itemPresent = false;
+		message = "Got a painting!";
+		StartCoroutine(wait());
+	}
+	
+	void OnGUI(){
+		if(displayMessage) {
+		 	GUI.Label (new Rect(Screen.width / 2, Screen.height / 4f, 100, 100), message);
+		}
+		if(triggered && itemPresent){
+			GUI.Label (new Rect(Screen.width / 2, Screen.height / 4f, 200, 200), "Press E to interact");	
+		}
+	}
+	
 	void OnTriggerEnter(Collider other){
 		triggered = true;
 	}
@@ -48,12 +72,21 @@ public class DoorOpenScript : MonoBehaviour {
 		triggered = false;
 	}
 	
-	void Update(){
+	void Update() {
 		if(triggered && Input.GetButtonDown("Use")){
-			if(!openDoor)
+			if(!openDoor && isDoor)
 				OpenDoor();
-			else
+			else if(openDoor && isDoor)
 				CloseDoor();
+			else {
+				PickUpPainting();
+			}
 		}
+	}
+	
+	IEnumerator wait(){
+		displayMessage = true;
+		yield return new WaitForSeconds(3);
+		displayMessage = false;
 	}
 }

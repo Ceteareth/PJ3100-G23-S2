feb42503e55af9a3ca947b1ed04d08428bb4fdc0
Torkill Strømmen <strtor10@nith.no>
2012-12-03 10:18:09 +0100

    Updated scripts, added new functions to AI

--- SpookyFollow.cs
+++ SpookyFollow.cs
@@ -3,31 +3,29 @@ using System.Collections;
 
 public class SpookyFollow : MonoBehaviour {
 	private Transform player;
+	private Transform light;
 	private Vector3 viewPlace;
+	private Vector3 nextPos;
 	private float distance;
-	public float followSpeed = 0.01F;
-	public float levitationHeight = 1.0F;
-	public float personalSpace = 1.7F;//Hvor langt unna playeren han stopper. 1,7 meter.
-	// Use this for initialization
+	public float moveSpeed = 0.015f;
+
 	void Start () {
 		player = GameObject.FindWithTag("Player").transform;
+		light = GameObject.FindWithTag ("Light").transform;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		distance = Vector3.Distance(transform.position, player.position);
 		viewPlace = player.position+player.forward;
-		if(distance > personalSpace)
+		if(distance>1.7)
 		{
-			if(Vector3.Distance(transform.position,viewPlace) > Vector3.Distance(transform.position,player.position))
+			if(!renderer.isVisible || light.light.intensity == 0)
 			{
-				transform.position = Vector3.Lerp(transform.position, player.position, followSpeed);
-				Vector3 temp = transform.position;
-				temp.y = Terrain.activeTerrain.SampleHeight(transform.position)+levitationHeight;
-				transform.position = temp;
+				transform.position = Vector3.Lerp(transform.position, player.position, moveSpeed);
+				nextPos = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position)+1.7F, transform.position.z);
+				transform.position = Vector3.Lerp(transform.position, nextPos, 1F);
 				transform.LookAt(new Vector3(player.position.x,1000,player.position.z));
 			}
 		}
-	
 	}
 }

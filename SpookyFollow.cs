af5517454f2166e1f5d556d36ee34c0c19d01436
Torkill Strømmen <strtor10@nith.no>
2012-12-03 10:53:15 +0100

    Updated followscript with variables instead of constants

--- SpookyFollow.cs
+++ SpookyFollow.cs
@@ -8,6 +8,8 @@ public class SpookyFollow : MonoBehaviour {
 	private Vector3 nextPos;
 	private float distance;
 	public float moveSpeed = 0.015f;
+	public float personalSpace = 1.7f;
+	public float levitationHeight = 1.0f;
 
 	void Start () {
 		player = GameObject.FindWithTag("Player").transform;
@@ -17,13 +19,13 @@ public class SpookyFollow : MonoBehaviour {
 	void Update () {
 		distance = Vector3.Distance(transform.position, player.position);
 		viewPlace = player.position+player.forward;
-		if(distance>1.7)
+		if(distance>personalSpace)
 		{
 			if(!renderer.isVisible || light.light.intensity == 0)
 			{
 				transform.position = Vector3.Lerp(transform.position, player.position, moveSpeed);
-				nextPos = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position)+1.7F, transform.position.z);
-				transform.position = Vector3.Lerp(transform.position, nextPos, 1F);
+				nextPos = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position)+personalSpace, transform.position.z);
+				transform.position = Vector3.Lerp(transform.position, nextPos, levitationHeight);
 				transform.LookAt(new Vector3(player.position.x,1000,player.position.z));
 			}
 		}

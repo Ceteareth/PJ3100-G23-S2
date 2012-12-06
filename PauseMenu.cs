90f42e2348f375d6582002735c46cbc4a381a978
Jorgen H Tomren <jorgenhtomren@gmail.com>
2012-12-06 11:49:57 +0100

    Pause Menu
    Pausemeny, funker for det meste. Må få disablet FPC-cam, og fjerne noen
    små "bugs"

---
+++ PauseMenu.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    bool isPaused = false;
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("escape") && !isPaused)
+        {
+            GetComponent<MouseLook>().enabled = false;
+            Screen.showCursor = true;
+            Time.timeScale = 0.0f;
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown("escape") && isPaused)
+        {
+            GetComponent<MouseLook>().enabled = true;
+            Screen.showCursor = false;
+            Time.timeScale = 1.0f;
+            isPaused = false;
+        }
+    }
+
+    void OnGUI()
+    {
+
+        if (isPaused)
+        {
+           
+            if (GUI.Button(new Rect (Screen.width/2, 120, 100, 56), "Quit"))
+            {
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect (Screen.width/2, 220, 100, 56), "Restart"))
+            {
+                
+               // Application.LoadLevel("SomeLevelHere");
+                GetComponent<MouseLook>().enabled = true;
+                Time.timeScale = 1.0f;
+                isPaused = false;
+            }
+            if (GUI.Button(new Rect (Screen.width/2, 320, 100, 56), "Continue"))
+            {
+                GetComponent<FPC>().enabled = false;
+                GetComponent<MouseLook>().enabled = true;
+                Screen.showCursor = false;
+                Time.timeScale = 1.0f;
+                isPaused = false;
+            }
+        }
+
+    }
+}

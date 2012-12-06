81488602ed8cede2759c851e3209572575517803
Jorgen H Tomren <jorgenhtomren@gmail.com>
2012-12-06 13:16:04 +0100

    PauseMeny
    Nå funker det med ett script. Må bare slenges på main camera.

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -5,10 +5,11 @@ public class PauseMenu : MonoBehaviour
 {
 
     bool isPaused = false;
-
+    
+    
     void Update()
     {
-
+        
         if (Input.GetKeyDown("escape") && !isPaused)
         {
             
@@ -17,8 +18,9 @@ public class PauseMenu : MonoBehaviour
             
         }
         else if (Input.GetKeyDown("escape") && isPaused)
+         
         {
-
+               
             Time.timeScale = 1.0f;
             isPaused = false;
         }
@@ -31,6 +33,8 @@ public class PauseMenu : MonoBehaviour
         {
             Screen.showCursor = true;
             GetComponent<MouseLook>().enabled = false;
+            GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = false;
+
            
             if (GUI.Button(new Rect (Screen.width/2, 120, 100, 56), "Quit"))
             {
@@ -39,14 +43,12 @@ public class PauseMenu : MonoBehaviour
             if (GUI.Button(new Rect (Screen.width/2, 220, 100, 56), "Restart"))
             {
                 
-               //Application.LoadLevel("test_project");
-                
+                //Application.LoadLevel("test_project"); 
                 Time.timeScale = 1.0f;
                 isPaused = false;
             }
             if (GUI.Button(new Rect (Screen.width/2, 320, 100, 56), "Continue"))
             {
-            
                 Time.timeScale = 1.0f;
                 isPaused = false;
             }
@@ -55,6 +57,7 @@ public class PauseMenu : MonoBehaviour
         {
             Screen.showCursor = false;
             GetComponent<MouseLook>().enabled = true;
+            GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = true;
         }
 
     }

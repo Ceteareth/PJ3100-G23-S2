2ac7d2c7f921e346fe642a4157381b57a6abafe1
Jorgen H Tomren <jorgenhtomren@gmail.com>
2012-12-06 12:30:30 +0100

    First Person Controller Script
    Eget script for kameraet til FPC

---
+++ FPCScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class FPCScript : MonoBehaviour
+{
+
+    bool isPaused = false;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("escape") && !isPaused)
+        {
+            
+            Time.timeScale = 0.0f;
+            isPaused = true;
+            
+        }
+        else if (Input.GetKeyDown("escape") && isPaused)
+        {
+
+            Time.timeScale = 1.0f;
+            isPaused = false;
+        }
+    }
+
+    void OnGUI()
+    {
+
+        if (isPaused)
+        {
+            Screen.showCursor = true;
+            GetComponent<MouseLook>().enabled = false;         
+
+        }
+        else if (!isPaused)
+        {
+            Screen.showCursor = false;
+            GetComponent<MouseLook>().enabled = true;
+        }
+
+    }
+}
